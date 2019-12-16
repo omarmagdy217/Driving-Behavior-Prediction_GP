945fe9d4ee03626d779f91c21c08d210d5607291
Ahmed Karaman <20889958+ahmedkrmn@users.noreply.github.com>
2019-12-16 23:37:47 +0200

    Add comments to script

--- Multithreaded Data Collection/main.py
+++ Multithreaded Data Collection/main.py
@@ -3,11 +3,15 @@ import threading
 
 
 def runSimulator():
+    # Run the "Blocks" environment located at ./Blocks/Blocks.exe
     os.system(os.getcwd() + "/Blocks/Blocks.exe")
 
 
 def main():
+    # Create a new thread for running the simulator
     simulatorThread = threading.Thread(target=runSimulator)
+
+    # Start the simulator thread
     simulatorThread.start()
 
 
