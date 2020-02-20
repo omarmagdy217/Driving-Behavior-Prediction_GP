9394b31f541571c158df3c091c373c6098abd5ab
omarmagdy217 <omr.magdy217@gmail.com>
2020-02-20 22:43:29 +0200

    Clean Repo Data

--- features extraction/features extraction for open source data/MainProject.py
+++ features extraction/features extraction for open source data/MainProject.py
@@ -2,12 +2,10 @@ import os
 import threading
 import Record_Data
 
-
 def runSimulator():
     # Run the "Blocks" environment located at ./Blocks/Blocks.exe
     #os.system('"' + os.getcwd() + "/SoundTrackApplication/WindowsNoEditor/MyProject.exe" + '"')
-    #os.system('"' + os.getcwd() + "/WindowsNoEditor/MRT.exe" + '"')
-    print("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh")
+    os.system('"' + os.getcwd() + "/WindowsNoEditor/MRT.exe" + '"')
 
 def main():
     # Create a new thread for running the simulator
