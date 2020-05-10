91f375d92c94c3f7a2178a98be8c70142298c832
omarahmed1111 <30423288+omarahmed1111@users.noreply.github.com>
2020-05-10 13:22:47 +0200

    Fix small bug in simulator performance main script

--- simulator performance scripts/main.py
+++ simulator performance scripts/main.py
@@ -7,8 +7,8 @@ import rank1_road2l as road4
 import rank1_road2r as road3
 import rank1_road3l as road6
 import rank1_road3r as road5
-import rank1_road4l as road7
-import rank1_road4r as road8
+import rank1_road4l as road8
+import rank1_road4r as road7
 
 log_path = path.expanduser('~\\Documents\\AirSim\\airsim_rec.txt')
 
@@ -19,7 +19,7 @@ if(roadNum==1):
     p,CP = road1.analyse(log_path)
     outfile = open("Rank1Road1ROutput.txt","w+")
     outfile.write("RANK: 1\n")
-    outfile.write("OVERALL PERFORMANCE: " + str(100-(p/5)*100) + "%\n")
+    outfile.write("OVERALL PERFORMANCE: " + str(100-((p-1)/4)*100) + "%\n")
     outfile.write("EPOCH TIMESTAMP(Every ~1000 ms)      Performance( Scale: 1-5)\n")
     for i in CP:
         outfile.write(str(i[0]) + "                        " + str(i[1]) + "\n")
@@ -28,7 +28,7 @@ elif(roadNum==2):
     p,CP = road2.analyse(log_path)
     outfile = open("Rank1Road1LOutput.txt","w+")
     outfile.write("RANK: 1\n")
-    outfile.write("OVERALL PERFORMANCE: " + str(100-(p/5)*100) + "%\n")
+    outfile.write("OVERALL PERFORMANCE: " + str(100-((p-1)/4)*100) + "%\n")
     outfile.write("EPOCH TIMESTAMP(Every ~1000 ms)      Performance( Scale: 1-5)\n")
     for i in CP:
         outfile.write(str(i[0]) + "                        " + str(i[1]) + "\n")        
@@ -37,7 +37,7 @@ elif(roadNum==3):
     p,CP = road3.analyse(log_path)
     outfile = open("Rank1Road2ROutput.txt","w+")
     outfile.write("RANK: 1\n")
-    outfile.write("OVERALL PERFORMANCE: " + str(100-(p/5)*100) + "%\n")
+    outfile.write("OVERALL PERFORMANCE: " + str(100-((p-1)/4)*100) + "%\n")
     outfile.write("EPOCH TIMESTAMP(Every ~1000 ms)      Performance( Scale: 1-5)\n")
     for i in CP:
         outfile.write(str(i[0]) + "                        " + str(i[1]) + "\n")        
@@ -46,7 +46,7 @@ elif(roadNum==4):
     p,CP = road4.analyse(log_path)
     outfile = open("Rank1Road2LOutput.txt","w+")
     outfile.write("RANK: 1\n")
-    outfile.write("OVERALL PERFORMANCE: " + str(100-(p/5)*100) + "%\n")
+    outfile.write("OVERALL PERFORMANCE: " + str(100-((p-1)/4)*100) + "%\n")
     outfile.write("EPOCH TIMESTAMP(Every ~1000 ms)      Performance( Scale: 1-5)\n")
     for i in CP:
         outfile.write(str(i[0]) + "                        " + str(i[1]) + "\n") 
@@ -55,7 +55,7 @@ elif(roadNum==5):
     p,CP = road5.analyse(log_path)
     outfile = open("Rank1Road3ROutput.txt","w+")
     outfile.write("RANK: 1\n")
-    outfile.write("OVERALL PERFORMANCE: " + str(100-(p/5)*100) + "%\n")
+    outfile.write("OVERALL PERFORMANCE: " + str(100-((p-1)/4)*100) + "%\n")
     outfile.write("EPOCH TIMESTAMP(Every ~1000 ms)      Performance( Scale: 1-5)\n")
     for i in CP:
         outfile.write(str(i[0]) + "                        " + str(i[1]) + "\n") 
@@ -64,25 +64,25 @@ elif(roadNum==6):
     p,CP = road6.analyse(log_path)
     outfile = open("Rank1Road3LOutput.txt","w+")
     outfile.write("RANK: 1\n")
-    outfile.write("OVERALL PERFORMANCE: " + str(100-(p/5)*100) + "%\n")
-    outfile.write("EPOCH TIMESTAMP(Every ~1000 ms)      Performance( Scale: 1-5)\n")
-    for i in CP:
-        outfile.write(str(i[0]) + "                        " + str(i[1]) + "\n")         
-
-elif(roadNum==8):
-    p,CP = road7.analyse(log_path)
-    outfile = open("Rank1Road4LOutput.txt","w+")
-    outfile.write("RANK: 1\n")
-    outfile.write("OVERALL PERFORMANCE: " + str(100-(p/5)*100) + "%\n")
+    outfile.write("OVERALL PERFORMANCE: " + str(100-((p-1)/4)*100) + "%\n")
     outfile.write("EPOCH TIMESTAMP(Every ~1000 ms)      Performance( Scale: 1-5)\n")
     for i in CP:
         outfile.write(str(i[0]) + "                        " + str(i[1]) + "\n")         
 
 elif(roadNum==7):
-    p,CP = road8.analyse(log_path)
+    p,CP = road7.analyse(log_path)
     outfile = open("Rank1Road4ROutput.txt","w+")
     outfile.write("RANK: 1\n")
-    outfile.write("OVERALL PERFORMANCE: " + str(100-(p/5)*100) + "%\n")
+    outfile.write("OVERALL PERFORMANCE: " + str(100-((p-1)/4)*100) + "%\n")
+    outfile.write("EPOCH TIMESTAMP(Every ~1000 ms)      Performance( Scale: 1-5)\n")
+    for i in CP:
+        outfile.write(str(i[0]) + "                        " + str(i[1]) + "\n")         
+
+elif(roadNum==8):
+    p,CP = road8.analyse(log_path)
+    outfile = open("Rank1Road4LOutput.txt","w+")
+    outfile.write("RANK: 1\n")
+    outfile.write("OVERALL PERFORMANCE: " + str(100-((p-1)/4)*100) + "%\n")
     outfile.write("EPOCH TIMESTAMP(Every ~1000 ms)      Performance( Scale: 1-5)\n")
     for i in CP:
         outfile.write(str(i[0]) + "                        " + str(i[1]) + "\n")         
